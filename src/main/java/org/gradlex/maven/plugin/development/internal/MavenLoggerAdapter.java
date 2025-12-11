01ed5d1ddf5438d4164134fa39c4cf0c6c779bd5
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-12-11 13:47:15 +0100

    Migrate build to shared convention plugins setup (#372)
    * Migrate build to shared convention plugins setup
    * Format code

--- src/main/java/org/gradlex/maven/plugin/development/internal/MavenLoggerAdapter.java
+++ src/main/java/org/gradlex/maven/plugin/development/internal/MavenLoggerAdapter.java
@@ -1,24 +1,10 @@
-/*
- * Copyright the GradleX team.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// SPDX-License-Identifier: Apache-2.0
 package org.gradlex.maven.plugin.development.internal;
 
 import org.gradle.api.logging.Logger;
 
-public final class MavenLoggerAdapter implements org.codehaus.plexus.logging.Logger, org.apache.maven.plugin.logging.Log {
+public final class MavenLoggerAdapter
+        implements org.codehaus.plexus.logging.Logger, org.apache.maven.plugin.logging.Log {
 
     private final Logger delegate;
 
